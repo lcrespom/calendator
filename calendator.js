01e188a94959a460159c3366f000363df5a403ed
Luis Crespo <lcrespom@gmail.com>
2018-10-17 12:50:29 +0200

    Fixed bug and added day id

--- calendator.js
+++ calendator.js
@@ -7,31 +7,34 @@ $(function() {
 	]
 	WEEKDAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-	function isLeapYear(year) {
-  		return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
-	}
-
 	function daysInMonth(month, year) {
 		return new Date(year, month + 1, 0).getDate()
 	}
 
-	function getMonthWeeks(mnum, year) {
-		// All months cover 5 weeks, except February,
-		// but only if it starts on a Monday and is not a leap year
-		if (mnum != 1 || isLeapYear(year)) return 5
-		let d = new Date(year, mnum, 1)
-		return d.getDay() == 1 ? 4 : 5
+	function dayOfWeek(date) {
+		let dow = date.getDay()
+		if (dow == 0) dow = 7	// Adjust week to Mon ... Sun pattern
+		return dow - 1
+	}
+
+	function getMonthWeeks(month, year) {
+		let dim = daysInMonth(month, year)
+		let dow = dayOfWeek(new Date(year, month, 1))
+		return Math.ceil((dim + dow) / 7)
 	}
 
 	function fillCalendarCells($month, mnum, year) {
 		let totalDays = getMonthWeeks(mnum, year) * 7
-		let d = new Date(year, mnum, 1)
-		let daynum = 2 - d.getDay()
+		let daynum = 1 - dayOfWeek(new Date(year, mnum, 1))
 		let dim = daysInMonth(mnum, year)
 		for (let i = 1; i <= totalDays; i++) {
 			let $daycell = $('<div class="cal-day"></div>')
-			if (daynum > 0 && daynum <= dim)
-				$daycell.append(`<div class="cal-cell">${daynum}</div></div>`)
+			if (daynum > 0 && daynum <= dim) {
+				id = `day-${year}-${mnum}-${daynum}`
+				$daycell.append(
+					`<div class="cal-cell" id="${id}">${daynum}</div></div>`
+				)
+			}
 			else
 				$daycell.addClass('cal-empty')
 			daynum++
