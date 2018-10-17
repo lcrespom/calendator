2d7c1cda4f0c97a894a219f7409db3cc01b861af
Luis Crespo <lcrespom@gmail.com>
2018-10-17 08:55:17 +0200

    Building all 12 months

--- calendator.js
+++ calendator.js
@@ -1,11 +1,43 @@
 $(function() {
-	
-	function fillCalendarCells($cal) {
+
+	MONTHS = [
+		'January', 'February', 'March', 'April',
+		'May', 'June', 'July', 'August',
+		'September', 'October', 'November', 'December'
+	]
+	WEEKDAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+	function fillCalendarCells($month) {
 		for (let i = 1; i <= 35; i++) {
-			html = `<div class="cal-day"><div class="cal-cell">${i}</div></div>`
-			$cal.append(html)
+			let html = `<div class="cal-day"><div class="cal-cell">${i}</div></div>`
+			$month.append(html)
 		}
 	}
 
-	fillCalendarCells($('.calendar'))
+	function addCalendar($cal, name, mnum, year) {
+		let $month = $('<div class="cal-month"></div>')
+		for (let weekday of WEEKDAYS)
+			$month.append(`<span class="cal-weekday">${weekday}</span>`)
+		fillCalendarCells($month)
+		let $mblock = $(`
+			<div class="cal-month-block">
+				<h2>${name}</h2>
+			</div>
+		`)
+		$mblock.append($month)
+		$cal.append($mblock)
+	}
+
+	let today = new Date()
+	let month = today.getMonth()
+	let year = today.getFullYear()
+	let $cal = $('#calendar')
+	for (let i = 0; i < 12; i++) {
+		let j = month + i
+		if (j == 12) year++
+		let mnum = j % 12
+		let name = `${MONTHS[mnum]} ${year}`
+		addCalendar($cal, name, mnum, year)
+	}
+
 })
