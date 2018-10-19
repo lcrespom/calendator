59ce50e0ac518cd4aeed1d8f550bf5a9ac18b142
Luis Crespo <lcrespom@gmail.com>
2018-10-19 09:35:53 +0200

    Dynamic building of event inputs

--- calendator.js
+++ calendator.js
@@ -64,51 +64,72 @@ $(function() {
 		$('#cal-add-event').click(_ => addEventInputs())
 	}
 
-	function formGroup(label, type = 'text', value = "") {
+	function formGroup(label, type = 'text', value = '', before = '', after = '') {
 		return $(`
 		<div class="form-group">
 			<label>${label}</label>
+			${before}
 			<input type="${type}" value="${value}" class="form-control"/>
+			${after}
 		</div>
 		`)
 	}
 
+	function randomColor() {
+		let [r, g, b] = hsl2rgb(Math.random(), 1, 0.5)
+		return rgb2hex(r, g, b)
+	}
+
 	function addEventInputs() {
-		// let $inputs = $(`<div class="cal-event-inputs"></div>`)
-		// $inputs.append(formGroup('Name'))
-		$('#events').append(`
-<hr/>
-<div class="cal-event-inputs">
-	<div class="form-group">
-		<label>Name</label>
-		<input type="text" class="form-control"/>
-	</div>
-	<div class="form-group">
-		<label>Color</label>
-		<input type="color" value="#ff0000" class="form-control">
-	</div>
-	<div class="form-group">
-		<label>Start</label>
-		<input type="date" class="form-control"/>
-	</div>
-	<div class="form-group">
-		<label>Duration</label>
-		<input type="number" class="form-control"/> days
-	</div>
-	<div class="form-group">
-		<label>Repeat</label>
-		every <input type="number" class="form-control"/> days
-	</div>
-	<div class="form-group">
-		<label>For</label>
-		<input type="number" class="form-control"/> times
-	</div>
-	<div class="form-group">
-		<label></label>
-		<button class="btn btn-sm btn-warning">Remove event</button>
-	</div>
-</div>
-`)
+		let $inputs = $(`<div class="cal-event-inputs"></div>`)
+		$inputs
+			.append(formGroup('Name'))
+			.append(formGroup('Color', 'color', randomColor()))
+			.append(formGroup('Start', 'date'))
+			.append(formGroup('Duration', 'number', '', '', 'days'))
+			.append(formGroup('Repeat', 'number', '', 'every', 'days'))
+			.append(formGroup('For', 'number', '', '', 'times'))
+			.append(`<div class="form-group">
+						<label />
+						<button class="btn btn-sm btn-warning">Remove event</button>
+					</div>`)
+		$('#event-list')
+			.prepend($inputs)
+			.prepend('<hr/>')
+	}
+
+
+	//-------------------- Misc utilities --------------------
+
+	// Obtained from https://stackoverflow.com/a/9493060/2342681
+	function hsl2rgb(h, s, l) {
+		let r, g, b
+		if (s == 0) {
+			r = g = b = l // achromatic
+		} else {
+			let hue2rgb = (p, q, t) => {
+				if (t < 0) t += 1
+				if (t > 1) t -= 1
+				if (t < 1 / 6) return p + (q - p) * 6 * t
+				if (t < 1 / 2) return q
+				if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+				return p
+			}
+			let q = l < 0.5 ? l * (1 + s) : l + s - l * s
+			let p = 2 * l - q
+			r = hue2rgb(p, q, h + 1 / 3)
+			g = hue2rgb(p, q, h)
+			b = hue2rgb(p, q, h - 1 / 3)
+		}
+		return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)]
+	}
+
+	function rgb2hex(r, g, b) {
+		let d2h = d => {
+			let h = d.toString(16)
+			return h.length > 1 ? h : '0' + h
+		}
+		return `#${d2h(r)}${d2h(g)}${d2h(b)}`
 	}
 
 	//-------------------- Main --------------------
